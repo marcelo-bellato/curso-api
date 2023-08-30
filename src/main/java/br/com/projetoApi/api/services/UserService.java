1a1d9ed64811236da0ad5c082390b31a69f6afab
marcelo-bellato <bellato.consultor@gmail.com>
2023-08-30 16:02:56 -0300

    endpoint findAll para User

--- src/main/java/br/com/projetoApi/api/services/UserService.java
+++ src/main/java/br/com/projetoApi/api/services/UserService.java
@@ -2,7 +2,9 @@ package br.com.projetoApi.api.services;
 
 import br.com.projetoApi.api.domain.User;
 
-public interface UserService {
+import java.util.List;
 
+public interface UserService {
     User findById(Integer id);
+    List<User> findAll();
 }
