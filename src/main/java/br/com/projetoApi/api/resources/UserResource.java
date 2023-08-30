75c2c62288f13e104eea8a5cc66bce9f501dd63d
marcelo-bellato <bellato.consultor@gmail.com>
2023-08-30 11:24:11 -0300

    classe user resource findById

--- src/main/java/br/com/projetoApi/api/resources/UserResource.java
+++ src/main/java/br/com/projetoApi/api/resources/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.projetoApi.api.resources;
 
 import br.com.projetoApi.api.domain.User;
+import br.com.projetoApi.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(new User(1, "Marcelo", "marcelo@mail.com", "123"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
