2416386451592ff14824459de093aed26cff1bd4
marcelo-bellato <bellato.consultor@gmail.com>
2023-08-31 14:26:12 -0300

    teste para classe user service metodo delete com excessao

---
+++ src/test/java/br/com/projetoApi/api/resources/UserResourceTest.java
@@ -0,0 +1,67 @@
+package br.com.projetoApi.api.resources;
+
+import br.com.projetoApi.api.domain.User;
+import br.com.projetoApi.api.domain.dto.UserDTO;
+import br.com.projetoApi.api.services.impl.UserServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserResourceTest {
+
+    public static final int ID = 1;
+    public static final String NAME = "Marcelo";
+    public static final String EMAIL = "marcelo@mail.com";
+    public static final String PASSWORD = "123";
+    public static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado";
+    public static final int INDEX = 0;
+    public static final String E_MAIL_JA_CADASTRADO_NO_SISTEMA = "E-mail já cadastrado no sistema";
+
+    private User user;
+    private UserDTO userDTO;
+
+    @InjectMocks
+    private UserResource resource;
+
+    @Mock
+    private UserServiceImpl service;
+
+    @Mock
+    private ModelMapper mapper;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    private void startUser() {
+        user = new User(ID, NAME, EMAIL, PASSWORD);
+        userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
+    }
+}
