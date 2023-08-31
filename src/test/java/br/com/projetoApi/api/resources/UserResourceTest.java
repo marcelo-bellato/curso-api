6795892831dba4fc92853c87c903b8f7f75e4618
marcelo-bellato <bellato.consultor@gmail.com>
2023-08-31 17:00:23 -0300

    Revert "import static"
    This reverts commit 2379346049ed7b657df70298e6bbb9f457abbd83.

--- src/test/java/br/com/projetoApi/api/resources/UserResourceTest.java
+++ src/test/java/br/com/projetoApi/api/resources/UserResourceTest.java
@@ -10,13 +10,6 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
-
-import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class UserResourceTest {
@@ -48,21 +41,7 @@ class UserResourceTest {
     }
 
     @Test
-    void whenFindByIdThenReturnSuccess() {
-        when(service.findById(anyInt())).thenReturn(user);
-        when(mapper.map(any(), any())).thenReturn(userDTO);
-
-        ResponseEntity<UserDTO> response = resource.findById(ID);
-
-        assertNotNull(response);
-        assertNotNull(response.getBody());
-        assertEquals(ResponseEntity.class, response.getClass());
-        assertEquals(UserDTO.class, response.getBody().getClass());
-
-        assertEquals(ID, response.getBody().getId());
-        assertEquals(NAME, response.getBody().getName());
-        assertEquals(EMAIL, response.getBody().getEmail());
-        assertEquals(PASSWORD, response.getBody().getPassword());
+    void findById() {
     }
 
     @Test
