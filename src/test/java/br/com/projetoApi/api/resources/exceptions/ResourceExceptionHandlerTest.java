c9acb7a4326de7a431bc6ff9112ab88930356cf6
marcelo-bellato <bellato.consultor@gmail.com>
2023-09-01 12:28:24 -0300

    teste para manipulador de excecoes metodo data Integrity Violation

--- src/test/java/br/com/projetoApi/api/resources/exceptions/ResourceExceptionHandlerTest.java
+++ src/test/java/br/com/projetoApi/api/resources/exceptions/ResourceExceptionHandlerTest.java
@@ -1,5 +1,6 @@
 package br.com.projetoApi.api.resources.exceptions;
 
+import br.com.projetoApi.api.services.exceptions.DataIntegratyViolationException;
 import br.com.projetoApi.api.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -48,5 +49,17 @@ class ResourceExceptionHandlerTest {
 
     @Test
     void dataIntegrityViolationException() {
+        ResponseEntity<StandardError> response = ecxeptionHandler
+                .dataIntegrityViolationException(
+                        new DataIntegratyViolationException(E_MAIL_JA_CADASTRADO),
+                        new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(E_MAIL_JA_CADASTRADO, response.getBody().getError());
+        assertEquals(400, response.getBody().getStatus());
     }
 }
